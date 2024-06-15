c912b8abc3dc4eff9c3a79de36322f5640374101
Carl Franklin <carl@appvnext.com>
2024-06-15 08:38:22 -0400

    Added Solution

---
+++ Solution/Puzzle-37/Puzzle-37/Puzzle-37/Program.cs
@@ -0,0 +1,37 @@
+using Puzzle_37.Client.Pages;
+using Puzzle_37.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddScoped<HttpClient>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Puzzle_37.Client._Imports).Assembly);
+
+app.MapGet("data", () => "Hello from the server!");
+
+app.Run();
---
+++ Solution/README.md
@@ -0,0 +1,16 @@
+# Blazor Puzzle #37
+
+## Minimal API Issue
+
+YouTube Video: https://youtu.be/ncsI4qbXfUo
+
+Blazor Puzzle Home Page: https://blazorpuzzle.com
+
+### The Challenge:
+
+This is a Global WebAssembly Blazor Web App that calls into a server-side minimal API endpoint. The app blows up when we try to retrieve the data as a string. Why?
+
+The Solution
+
+
+
